3174d8ef4be53eb1d52d6c215f1522afd34c851e
Diego Llopis <diego.llopis1@gmail.com>
2021-11-09 16:52:38 -0300

    estrutura de pastas básica

---
+++ desafioMoviesTMDB/desafioMoviesTMDB/App/desafioMoviesTMDBApp.swift
@@ -0,0 +1,17 @@
+//
+//  desafioMoviesTMDBApp.swift
+//  desafioMoviesTMDB
+//
+//  Created by Diego Llopis on 09/11/21.
+//
+
+import SwiftUI
+
+@main
+struct desafioMoviesTMDBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MainView()
+        }
+    }
+}
---
+++ desafioMoviesTMDB/desafioMoviesTMDB/Views/MainView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  desafioMoviesTMDB
+//
+//  Created by Diego Llopis on 09/11/21.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainView()
+    }
+}
