4ed93b79f3232952b4f56299172866f73fcbacc1
Diego Llopis <diego.llopis1@gmail.com>
2021-11-09 17:27:43 -0300

    adicionando AddListButtonView

---
+++ desafioMoviesTMDB/desafioMoviesTMDB/Views/AddListButtonView.swift
@@ -0,0 +1,40 @@
+//
+//  AddListButtonView.swift
+//  desafioMoviesTMDB
+//
+//  Created by Diego Llopis on 09/11/21.
+//
+
+import SwiftUI
+
+struct AddListButtonView: View {
+    
+    @State private var add: Bool = false
+        
+    var body: some View {
+    
+        Button {
+            self.add = true
+        } label: {
+                Text(add ? "Adicionado às Minhas Listas" : "Adicionar às Minhas Listas")
+                    .frame(maxWidth: .infinity)
+                    .padding(.vertical, 10)
+        }
+        .foregroundColor(add ? .black : .white)
+        .background(add ? Color.white.opacity(1) : Color.white.opacity(0))
+        .overlay(RoundedRectangle(cornerRadius: 8).stroke(Color.white, lineWidth: 2))
+        .cornerRadius(8)
+        .padding(.horizontal)
+    }
+}
+
+struct AddListButton_Previews: PreviewProvider {
+    static var previews: some View {
+        
+        ZStack {
+            Color.black
+            AddListButtonView()
+        }
+        
+    }
+}
